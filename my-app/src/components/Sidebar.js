77d7e237dc30d27cd0eaac34c9ae98436103affb
Matt Manning <mtmanning93@hotmail.co.uk>
2023-08-31 06:53:59 +0000

    Fix return syntax

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -2,11 +2,13 @@ import React from "react";
 import css from "./css/Sidebar.module.css"
 
 const SideBar = () => {
-    return  <div className={css.sidebar}>
-                <a>My Photos</a>
-                <a>My Illustrations</a>
-                <a>My Paintings</a>
-            </div>
+    return (
+        <div className={css.sidebar}>
+            <a>My Photos</a>
+            <a>My Illustrations</a>
+            <a>My Paintings</a>
+        </div>
+    );
 }
 
 export default SideBar
