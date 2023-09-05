afa393f60892c413cf671295e36821f332559fe1
Matt Manning <mtmanning93@hotmail.co.uk>
2023-09-05 04:36:03 +0000

    Use componentDidMount to complete challenge, add 2 sec timeout to loader

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,8 @@
 import css from './App.module.css';
 import Content from './components/Content';
+import Loader from './components/Loader';
 import NavBarForm from './components/NavBarForm';
-import NavBarSimple from './components/NavBarSimple';
+// import NavBarSimple from './components/NavBarSimple';
 import SideBar from './components/Sidebar';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <SideBar />
       <NavBarForm />
       <Content />
+      {/* <Loader /> */}
     </div>
   );
 }
--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react'
 import {savedPosts} from "../posts.json";
 import css from "./css/Content.module.css";
 import Postitems from './Postitems';
+import Loader from './Loader';
 
 export class Content extends Component {
     constructor(props) {
         super(props)
         
         this.state = {
+            isLoaded: false
         }
+    }
+
+    componentDidMount() {
+        this.getData()
+    }
+
+    getData () {
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true
+            })
+        }, 2000)
     }
 
     render() {
@@ -18,7 +32,9 @@ export class Content extends Component {
                     <h1>My Posts</h1>
                 </div>
                 <div className={css.SearchResults}>
-                    <Postitems savedPosts = {savedPosts} /> 
+                    {
+                    this.state.isLoaded ? <Postitems savedPosts = {savedPosts} /> : <Loader />
+                    }
                     {/* {
                         savedPosts.map((post) => {
                             return (
